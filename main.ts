848ffce869a60da5363d76da4c1cae03b56a40ab
Germanhorn12 <108017639+Germanhorn12@users.noreply.github.com>
2022-06-23 13:18:42 +0100

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`cactus`, function (sprite, location) {
+    if (invincibility == 0) {
+        info.changeLifeBy(-1)
+        invincibility = 1
+    }
+})
+let invincibility = 0
 scene.setBackgroundColor(7)
 tiles.setCurrentTilemap(tilemap`level1`)
 let epicGuy = sprites.create(img`
@@ -39,3 +46,11 @@ let First_guy = sprites.create(img`
 tiles.placeOnTile(First_guy, tiles.getTileLocation(63, 63))
 controller.moveSprite(epicGuy)
 scene.cameraFollowSprite(epicGuy)
+info.setLife(6)
+invincibility = 0
+forever(function () {
+    if (invincibility == 1) {
+        pause(1000)
+        invincibility = 0
+    }
+})
