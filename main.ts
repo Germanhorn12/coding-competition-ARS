9c7acf9c089b0d82a958d4dea866bff281365764
redunicornchocolate <108017640+redunicornchocolate@users.noreply.github.com>
2022-06-23 13:01:31 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -18,5 +18,24 @@ let epicGuy = sprites.create(img`
     . . . . f 2 f . f 2 f . . . . . 
     . . . . f f f . f f f . . . . . 
     `, SpriteKind.Player)
+let First_guy = sprites.create(img`
+    . . f f f f f f f f f f f . . . 
+    . . f 6 6 6 6 6 6 6 6 6 6 f . . 
+    . . f f f f f f f f f f f f . . 
+    . . f f f f f f f f f 1 f . . . 
+    . . f f f f f f f f 1 1 f . . . 
+    . . f f f f f f f f f f f . . . 
+    . . . f f f f f f f f f 1 . . . 
+    . . f f f 6 6 6 6 6 f f f . . . 
+    . . f 6 f 6 6 6 6 6 f 6 f . . . 
+    . . f 6 f 6 6 6 6 6 f 6 f . . . 
+    . . f f f 6 6 6 6 6 f f f . . . 
+    . . . . f f f f f f f . . . . . 
+    . . . . f 6 f . f 6 f . . . . . 
+    . . . . f 6 f . f 6 f . . . . . 
+    . . . . f 6 f . f 6 f . . . . . 
+    . . . . f f f . f f f . . . . . 
+    `, SpriteKind.Player)
+tiles.placeOnTile(First_guy, tiles.getTileLocation(63, 63))
 controller.moveSprite(epicGuy)
 scene.cameraFollowSprite(epicGuy)
